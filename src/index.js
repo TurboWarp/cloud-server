b6516f3074eb4fa5ef51f4a6b2747ca910e0b648
Thomas Weber <tommyweber33@gmail.com>
2020-06-06 00:34:04 -0500

    Add public directory support
    This changes HTTP requests to serve static files instead of remaining open but never sending data.
    
    Also removes exit handling that seems to be very broken

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 const http = require('http');
 const process = require('process');
+const static = require('node-static');
 
 const logger = require('./logger');
 const config = require('./config');
 
 const wss = require('./server');
-const server = http.createServer();
+const fileServer = new static.Server('./public', {
+  serverInfo: 'https://github.com/forkphorus/cloud-server',
+});
+const server = http.createServer(function(req, res) {
+  // Serve static files
+  req.addListener('end', function () {
+    fileServer.serve(req, res);
+  }).resume();
+});
 
 server.on('upgrade', function upgrade(request, socket, head) {
+  // Forward these requests to the WebSocket server.
   wss.handleUpgrade(request, socket, head, function done(ws) {
     wss.emit('connection', ws, request);
   });
+});
+
+server.on('close', function() {
+  // TODO: this code never seems to actually run
+  logger.info('Server closing');
+  wss.close();
 });
  
 server.listen(config.port, function() {
   logger.info('Server started on port: ' + config.port);
 });
-
-server.on('close', function() {
-  logger.info('Server closing');
-  wss.close();
-});
-
-function exit() {
-  server.close();
-}
-
-process.on('exit', exit);
-process.on('SIGINT', exit);
-process.on('SIGTERM', exit);
