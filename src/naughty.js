6ef0cb32b185c3a857144cdaa4640576b11486c1
Thomas Weber <tommyweber33@gmail.com>
2020-06-14 11:45:56 -0500

    Update bad word detection, add some username tests
    - change word detection to use a json list
    - optimize isSafe: indexOf, manual loop
    - add a whole bunch of usernames to the username tests

--- src/naughty.js
+++ src/naughty.js
@@ -1,28 +1,27 @@
-// This file contains many words that some may find disturbing.
-
-const NAUGHTY_WORDS = [
-  // All words here MUST be lowercase.
-  // TODO: more words
-  // TODO: see if we can store these somewhere else that might be a bit less obvious
-  'bitch',
-  'fuck',
-  'shit',
-];
+const NAUGHTY_WORDS = require('./badwords.json').words;
 
 // Ignore anything non-alphabetical
 const IGNORE = /[^a-z]/g;
 
 /**
+ * @param {string} text
+ */
+function simplify(text) {
+  return text.toLowerCase().replace(IGNORE, '');
+}
+
+/**
  * Determine whether a given string of text is probably safe for most audiences.
  * Note that this is not foolproof.
  * @param {string} text The text to scan
- * @returns {boolean} true if the text is probably safe, false if the text is definitely unsafe
+ * @returns {boolean} true if the text is probably safe, false if the text is probably unsafe
  */
 function isSafe(text) {
-  text = text.toLowerCase().replace(IGNORE, '');
+  text = simplify(text);
 
-  for (const word of NAUGHTY_WORDS) {
-    if (text.includes(word)) {
+  const length = NAUGHTY_WORDS.length;
+  for (var i = 0; i < length; i++) {
+    if (text.indexOf(NAUGHTY_WORDS[i]) !== -1) {
       return false;
     }
   }
