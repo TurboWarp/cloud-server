c1575d19c3e98b9223a047eeb1edf357a565a7f8
Thomas Weber <tommyweber33@gmail.com>
2020-07-02 13:20:36 -0500

    Increase variable value max length

--- src/validators.js
+++ src/validators.js
@@ -6,7 +6,7 @@ const VARIABLE_NAME_CLOUD_PREFIX = '☁ ';
 const VARIABLE_NAME_MAX_LENGTH = 512;
 
 /** The maximum length of a variable's value. */
-const VALUE_MAX_LENGTH = 1024;
+const VALUE_MAX_LENGTH = 2048;
 
 /** Maximum length of usernames, inclusive. */
 const USERNAME_MAX_LENGTH = 20;
