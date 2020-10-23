8916a702874d59c85fe47bb60c63deaecc227585
Thomas Weber <weber_t@mailbox.org>
2020-10-22 19:06:15 -0500

    Include timestamps in logs

--- src/logger.js
+++ src/logger.js
@@ -4,12 +4,16 @@ require('winston-daily-rotate-file');
 const config = require('./config');
 const environment = require('./environment');
 
+const format = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf(({ level, message, timestamp }) => {
+    return `[${timestamp}] ${level}: ${message}`;
+  })
+);
+
 const logger = winston.createLogger({
   level: environment.isDevelopment ? 'debug' : 'info',
-  format: winston.format.combine(
-    winston.format.splat(),
-    winston.format.simple()
-  ),
+  format: format
 });
 
 logger.add(new winston.transports.DailyRotateFile(config.logging.rotation));
@@ -17,9 +21,10 @@ logger.add(new winston.transports.DailyRotateFile(config.logging.rotation));
 if ((environment.isDevelopment || config.logging.forceEnableConsoleLogging) && !environment.isTest) {
   logger.add(new winston.transports.Console({
     format: winston.format.combine(
+      // Include color when logging to console, not when logging to file (results in ugly escape codes)
       winston.format.colorize(),
-      winston.format.simple(),
-    ),
+      format
+    )
   }));
 }
 
