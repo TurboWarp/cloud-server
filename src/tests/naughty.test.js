903e5252079b44ef9646f653d20b2703487a6053
Thomas Weber <weber_t@mailbox.org>
2021-02-21 22:41:52 -0600

    Remove some more naughty words

--- src/tests/naughty.test.js
+++ src/tests/naughty.test.js
@@ -4,12 +4,6 @@ const naughty = require('../naughty');
 
 test('naughty word detector', () => {
   expect(naughty('griffpatch')).toBe(false);
-  expect(naughty('Hell-o :)')).toBe(false);
-  expect(naughty('fuck')).toBe(true);
-  expect(naughty('f_u_c_-k')).toBe(true);
-  expect(naughty('-fu_c_k')).toBe(true);
-  expect(naughty('FUCK')).toBe(true);
-  expect(naughty('bitchute')).toBe(true);
   expect(naughty('ceebee')).toBe(true);
   expect(naughty('iloveceebee')).toBe(false);
   expect(naughty('')).toBe(false);
