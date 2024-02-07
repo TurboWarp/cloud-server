03969c63246ca2f0f243bd4a9383ff8eaa55809d
Muffin <muffin@mailbox.org>
2024-02-07 15:53:19 -0600

    Raise MAX_ROOMS

--- src/RoomList.js
+++ src/RoomList.js
@@ -7,7 +7,7 @@ const JANITOR_INTERVAL = 1000 * 60;
 /** Time a room must be empty for before it may be removed by the janitor. */
 const JANITOR_THRESHOLD = 1000 * 60 * 60;
 /** Maximum amount of rooms that can exist at once. Empty rooms are included in this limit. */
-const MAX_ROOMS = 1024;
+const MAX_ROOMS = 16384;
 
 /**
  * @typedef {import('./Room').RoomID} RoomID
